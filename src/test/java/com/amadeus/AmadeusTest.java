fb56c183be27d428148df7e967e7f2844e6f4e8b
Anthony Roux <anthony.roux@amadeus.com>
2018-10-09 19:35:07 +0200

    fix broken test

--- src/test/java/com/amadeus/AmadeusTest.java
+++ src/test/java/com/amadeus/AmadeusTest.java
@@ -50,7 +50,7 @@ public class AmadeusTest {
   }*/
 
   @Test public void testVersion() {
-    assertEquals("should have a version number", Amadeus.VERSION, "1.1.0");
+    assertEquals("should have a version number", Amadeus.VERSION, "1.2.0");
   }
 
 }
