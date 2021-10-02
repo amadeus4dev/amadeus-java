822d16262d7856baa5fa2720afcf594bc9e96dbf
sdutta <siddhartha.dutta@amadeus.com>
2021-10-02 11:22:10 +0530

    added file support for trip parser client

--- src/main/java/com/amadeus/travel/TripParser.java
+++ src/main/java/com/amadeus/travel/TripParser.java
@@ -6,6 +6,11 @@ import com.amadeus.exceptions.ResponseException;
 import com.amadeus.resources.Resource;
 import com.amadeus.resources.TripDetail;
 import com.google.gson.JsonObject;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * <p>
@@ -65,6 +70,29 @@ public class TripParser {
    * @throws ResponseException when an exception occurs
    */
   public TripDetail post(String body) throws ResponseException {
+    Response response = client.post("/v3/travel/trip-parser", body);
+    return (TripDetail) Resource.fromObject(response, TripDetail.class);
+  }
+
+  /**
+   * <p>
+   * The Trip Parser API parses the content of a document to extract trip information.
+   * Documents can be of type: PDF, XML, JSON, JPG, EML etc.
+   * </p>
+   *
+   * <pre>
+   * amadeus.travel.tripParser.post(file);</pre>
+   *
+   * @param file the file to send to the API as a File
+   * @return an API resource
+   * @throws ResponseException when an exception occurs
+   */
+  public TripDetail post(File file) throws ResponseException, IOException {
+    // Base64 encode file and create request body
+    String b64Encoded = Base64.encode(Files.readAllBytes(file.toPath()));
+    JsonObject body = new JsonObject();
+    body.addProperty("payload", b64Encoded);
+
     Response response = client.post("/v3/travel/trip-parser", body);
     return (TripDetail) Resource.fromObject(response, TripDetail.class);
   }
