c82030a3687060b26f428357c8bea8ca86b0da20
Anna Tsolakou <anna.tsolakou@amadeus.com>
2021-11-18 15:57:57 +0100

    release 5.8.1

--- src/test/java/com/amadeus/AmadeusTest.java
+++ src/test/java/com/amadeus/AmadeusTest.java
@@ -52,7 +52,7 @@ public class AmadeusTest {
   }*/
 
   @Test public void testVersion() {
-    assertEquals("should have a version number", Amadeus.VERSION, "5.8.0");
+    assertEquals("should have a version number", Amadeus.VERSION, "5.8.1");
   }
 
 }
