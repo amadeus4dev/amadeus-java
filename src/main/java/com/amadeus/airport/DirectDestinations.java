9576d93293707fd042f13f4460d76ad2814ff0bc
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-02-08 16:25:13 +0100

    Add support for Airport Routes V1 - Xianqi

---
+++ src/main/java/com/amadeus/airport/DirectDestinations.java
@@ -0,0 +1,62 @@
+package com.amadeus.airport;
+
+import com.amadeus.Amadeus;
+import com.amadeus.Params;
+import com.amadeus.Response;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.DirectDestination;
+import com.amadeus.resources.Resource;
+
+/**
+ * <p>
+ *   A namespaced client for the
+ *   <code>/v1/airport/direct-destinations</code> endpoints.
+ * </p>
+ *
+ * <p>
+ *   Access via the Amadeus client object.
+ * </p>
+ *
+ * <pre>
+ * Amadeus amadeus = Amadeus.builder("clientId", "secret").build();
+ * amadeus.airport.flightDestinations;</pre>
+ */
+public class DirectDestinations {
+
+    private Amadeus client;
+
+    /**
+     * Constructor.
+     * @hide
+     */
+    public DirectDestinations(Amadeus client) {
+        this.client = client;
+    }
+
+    /**
+     * <p>
+     *    Find all destinations served by a given airport.
+     * </p>
+     *
+     * <pre>
+     * amadeus.airport.directDestinations.get(Params.with("origin", "LHR"));
+     * </pre>
+     *
+     * @param params the parameters to send to the API
+     * @return an API response object
+     * @throws ResponseException when an exception occurs
+     */
+    public DirectDestination[] get(Params params) throws ResponseException {
+        Response response = client.get("/v1/airport/direct-destinations", params);
+        return (DirectDestination[]) Resource.fromArray(response, DirectDestination[].class);
+    }
+
+    /**
+     * Convenience method for calling <code>get</code> without any parameters.
+     * @see DirectDestinations#get()
+     */
+    public DirectDestination[] get() throws ResponseException {
+        return get(null);
+    }
+
+}
---
+++ src/main/java/com/amadeus/resources/DirectDestination.java
@@ -0,0 +1,21 @@
+package com.amadeus.resources;
+
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * An DirectDestination object as returned by the DirectDestinations API.
+ * @see com.amadeus.airport.DirectDestinations#get()
+ */
+
+@ToString
+public class DirectDestination extends Resource {
+
+    protected DirectDestination() {}
+
+    private @Getter String type;
+    private @Getter String subtype;
+    private @Getter String name;
+    private @Getter String iataCode;
+
+}
