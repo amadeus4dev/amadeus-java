589ae936419717cb5a0de6aaa686bc2bd0caad33
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-02-10 17:30:19 +0100

    Add support for Flight Availabilities Search API - correct indentation

--- src/main/java/com/amadeus/resources/FlightAvailabilitySearch.java
+++ src/main/java/com/amadeus/resources/FlightAvailabilitySearch.java
@@ -11,59 +11,54 @@ import lombok.ToString;
  */
 @ToString
 public class FlightAvailabilitySearch extends Resource {
-	protected FlightAvailabilitySearch() {}
+  protected FlightAvailabilitySearch() {}
 
-	private @Getter String type;
+  private @Getter String type;
+  private @Getter String id;
+  private @Getter String originalDestinationId;
+  private @Getter String source;
+  private @Getter Boolean instantTicketingRequired;
+  private @Getter Boolean paymentCardRequired;
+  private @Getter String duration;
+  private @Getter SearchSegment[] segments;
+
+  @ToString
+  public class SearchSegment {
+	protected SearchSegment() {}
+
 	private @Getter String id;
-	private @Getter String originalDestinationId;
-	private @Getter String source;
-	private @Getter Boolean instantTicketingRequired;
-	private @Getter Boolean paymentCardRequired;
-	private @Getter String duration;
-	private @Getter SearchSegment[] segments;
-	
-	@ToString
-	public class SearchSegment {
-		protected SearchSegment() {
-		}
-		
-		private @Getter String id;
-		private @Getter int numberOfStops;
-		private @Getter boolean blacklistedInEU;
-		private @Getter AirportInfo departure;
-		private @Getter AirportInfo arrival;
-		private @Getter String carrierCode;
-		private @Getter String number;
-		private @Getter Aircraft aircraft;
-		private @Getter AvailabilityClass[] availabilityClasses;
-	}
-	
-	@ToString
-	public class AirportInfo {
-		protected AirportInfo() {
-		}
-		
-		private @Getter String iataCode;
-		private @Getter String terminal;
-		private @Getter String at;
-	}
-	
-	@ToString
-	public class Aircraft {
-		protected Aircraft() {
-		}
-		
-		private @Getter String code;
-	}
-	
-	@ToString
-	public class AvailabilityClass {
-		protected AvailabilityClass() {
-		}
-		
-		private @Getter int numberOfBookableSeats;
-		@SerializedName("class")
-		private @Getter String segmentClass;
-	}
-	
+	private @Getter int numberOfStops;
+	private @Getter boolean blacklistedInEU;
+	private @Getter AirportInfo departure;
+	private @Getter AirportInfo arrival;
+	private @Getter String carrierCode;
+	private @Getter String number;
+	private @Getter Aircraft aircraft;
+	private @Getter AvailabilityClass[] availabilityClasses;
+  }
+
+  @ToString
+  public class AirportInfo {
+	protected AirportInfo() { }
+
+	private @Getter String iataCode;
+	private @Getter String terminal;
+	private @Getter String at;
+  }
+
+  @ToString
+  public class Aircraft {
+	protected Aircraft() {}
+
+	private @Getter String code;
+  }
+
+  @ToString
+  public class AvailabilityClass {
+	protected AvailabilityClass() {}
+
+	private @Getter int numberOfBookableSeats;
+	@SerializedName("class")
+	private @Getter String segmentClass;
+  }
 }
--- src/main/java/com/amadeus/shopping/FlightAvailabilitiesSearch.java
+++ src/main/java/com/amadeus/shopping/FlightAvailabilitiesSearch.java
@@ -23,91 +23,91 @@ import com.google.gson.JsonObject;
  * amadeus.shopping.flightAvailabilitiesSearch;</pre>
  */
 public class FlightAvailabilitiesSearch {
-	private Amadeus client;
-	
-	/**
-	 * Constructor.
-	 *
-	 * @hide
-	 */
-	public FlightAvailabilitiesSearch(Amadeus client) {
-		this.client = client;
-	}
-	
-	/**
-	 * <p>
-	 *   The Amadeus Flight Availability API provides a list of flights with seats for sale,
-	 *   and the quantity of seats available in different fare classes on a given itinerary.
-	 *   Additional information such as carrier and aircraft information,
-	 *   the departure and arrival terminals, schedule, and route are also provided.
-	 * </p>
-	 *
-	 * <pre>
-	 * amadeus.shopping.flightAvailabilitiesSearch.get(params);</pre>
-	 *
-	 * @param params the parameters to send to the API
-	 * @return an API resource
-	 * @throws ResponseException when an exception occurs
-	 */
-	public FlightAvailabilitySearch[] get(Params params) throws ResponseException {
-		Response response = client.get("/v1/shopping/availability/flight-availabilities", params);
-		return (FlightAvailabilitySearch[]) Resource.fromArray(response, FlightAvailabilitySearch[].class);
-	}
-	
-	/**
-	 * Convenience method for calling <code>get</code> without any parameters.
-	 * @see FlightAvailabilitiesSearch#get()
-	 */
-	public FlightAvailabilitySearch[] get() throws ResponseException {
-		return get(null);
-	}
-	
-	/**
-	 * <p>
-	 *   The Amadeus Flight Availability API provides a list of flights with seats for sale,
-	 *   and the quantity of seats available in different fare classes on a given itinerary.
-	 *   Additional information such as carrier and aircraft information,
-	 *   the departure and arrival terminals, schedule, and route are also provided.
-	 * </p>
-	 *
-	 * <pre>
-	 * amadeus.shopping.flightAvailabilitiesSearch.post(body);</pre>
-	 *
-	 * @param body the parameters to send to the API as a JsonObject
-	 * @return an API resource
-	 * @throws ResponseException when an exception occurs
-	 */
-	public FlightAvailabilitySearch[] post(JsonObject body) throws ResponseException {
-		Response response = client.post("/v1/shopping/availability/flight-availabilities", body);
-		return (FlightAvailabilitySearch[]) Resource.fromArray(response, FlightAvailabilitySearch[].class);
-	}
-	
-	/**
-	 * <p>
-	 *   The Amadeus Flight Availability API provides a list of flights with seats for sale,
-	 *   and the quantity of seats available in different fare classes on a given itinerary.
-	 *   Additional information such as carrier and aircraft information,
-	 *   the departure and arrival terminals, schedule, and route are also provided.
-	 * </p>
-	 *
-	 * <pre>
-	 * amadeus.shopping.flightAvailabilitiesSearch.post(body);</pre>
-	 *
-	 * @param body the parameters to send to the API as a String
-	 * @return an API resource
-	 * @throws ResponseException when an exception occurs
-	 */
-	public FlightAvailabilitySearch[] post(String body) throws ResponseException {
-		Response response = client.post("/v1/shopping/availability/flight-availabilities", body);
-		return (FlightAvailabilitySearch[]) Resource.fromArray(response, FlightAvailabilitySearch[].class);
-	}
-	
-	/**
-	 * Convenience method for calling <code>post</code> without any parameters.
-	 *
-	 * @see FlightAvailabilitiesSearch#post()
-	 */
-	public FlightAvailabilitySearch[] post() throws ResponseException {
-		return post((String) null);
-	}
+  private Amadeus client;
+
+  /**
+   * Constructor.
+   *
+   * @hide
+   */
+  public FlightAvailabilitiesSearch(Amadeus client) {
+    this.client = client;
+  }
+
+  /**
+   * <p>
+   *   The Amadeus Flight Availability API provides a list of flights with seats for sale,
+   *   and the quantity of seats available in different fare classes on a given itinerary.
+   *   Additional information such as carrier and aircraft information,
+   *   the departure and arrival terminals, schedule, and route are also provided.
+   * </p>
+   *
+   * <pre>
+   * amadeus.shopping.flightAvailabilitiesSearch.get(params);</pre>
+   *
+   * @param params the parameters to send to the API
+   * @return an API resource
+   * @throws ResponseException when an exception occurs
+   */
+  public FlightAvailabilitySearch[] get(Params params) throws ResponseException {
+    Response response = client.get("/v1/shopping/availability/flight-availabilities", params);
+    return (FlightAvailabilitySearch[]) Resource.fromArray(response, FlightAvailabilitySearch[].class);
+  }
+
+  /**
+   * Convenience method for calling <code>get</code> without any parameters.
+   * @see FlightAvailabilitiesSearch#get()
+   */
+  public FlightAvailabilitySearch[] get() throws ResponseException {
+    return get(null);
+  }
+
+  /**
+   * <p>
+   *   The Amadeus Flight Availability API provides a list of flights with seats for sale,
+   *   and the quantity of seats available in different fare classes on a given itinerary.
+   *   Additional information such as carrier and aircraft information,
+   *   the departure and arrival terminals, schedule, and route are also provided.
+   * </p>
+   *
+   * <pre>
+   * amadeus.shopping.flightAvailabilitiesSearch.post(body);</pre>
+   *
+   * @param body the parameters to send to the API as a JsonObject
+   * @return an API resource
+   * @throws ResponseException when an exception occurs
+   */
+  public FlightAvailabilitySearch[] post(JsonObject body) throws ResponseException {
+    Response response = client.post("/v1/shopping/availability/flight-availabilities", body);
+    return (FlightAvailabilitySearch[]) Resource.fromArray(response, FlightAvailabilitySearch[].class);
+  }
+
+  /**
+   * <p>
+   *   The Amadeus Flight Availability API provides a list of flights with seats for sale,
+   *   and the quantity of seats available in different fare classes on a given itinerary.
+   *   Additional information such as carrier and aircraft information,
+   *   the departure and arrival terminals, schedule, and route are also provided.
+   * </p>
+   *
+   * <pre>
+   * amadeus.shopping.flightAvailabilitiesSearch.post(body);</pre>
+   *
+   * @param body the parameters to send to the API as a String
+   * @return an API resource
+   * @throws ResponseException when an exception occurs
+   */
+  public FlightAvailabilitySearch[] post(String body) throws ResponseException {
+    Response response = client.post("/v1/shopping/availability/flight-availabilities", body);
+    return (FlightAvailabilitySearch[]) Resource.fromArray(response, FlightAvailabilitySearch[].class);
+  }
+
+  /**
+  * Convenience method for calling <code>post</code> without any parameters.
+  *
+  * @see FlightAvailabilitiesSearch#post()
+  */
+  public FlightAvailabilitySearch[] post() throws ResponseException {
+    return post((String) null);
+  }
 }
