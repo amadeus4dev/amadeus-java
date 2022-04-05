15869dcfe85eb9bb145c44c675ba330978ab54d6
Juan Antonio Brena Moral <juanantonio.brenamoral@amadeus.com>
2022-04-05 15:10:18 +0200

    Adding IT test for poi by id

---
+++ src/test/java/com/amadeus/referenceData/locations/PointOfInterestIT.java
@@ -0,0 +1,74 @@
+package com.amadeus.referenceData.locations;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.assertj.core.api.Java6BDDAssertions.then;
+
+import com.amadeus.Amadeus;
+import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.PointOfInterest;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PointOfInterestIT {
+
+  WireMockServer wireMockServer;
+
+  private Amadeus amadeus;
+
+  /**
+   * In every tests, we will authenticate.
+   */
+  @BeforeEach
+  public void setup() {
+    wireMockServer = new WireMockServer(8080);
+    wireMockServer.start();
+
+    //https://developers.amadeus.com/self-service/apis-docs/guides/authorization-262
+    String address = "/v1/security/oauth2/token"
+        + "?grant_type=client_credentials&client_secret=DEMO&client_id=DEMO";
+    wireMockServer.stubFor(post(urlEqualTo(address))
+        .willReturn(aResponse().withHeader("Content-Type", "application/json")
+        .withStatus(200)
+        .withBodyFile("auth_ok.json")));
+
+    amadeus = Amadeus
+      .builder("DEMO", "DEMO")
+      .setHost("localhost")
+      .setPort(8080)
+      .setSsl(false)
+      .setLogLevel("debug")
+      .build();
+  }
+
+  @AfterEach
+  public void teardown() {
+    wireMockServer.stop();
+  }
+
+  @Test
+  public void given_client_when_call_point_of_interest_by_id_without_params_then_ok()
+      throws ResponseException {
+
+    //Given
+    String id = "9CB40CB5D0";
+
+    //https://developers.amadeus.com/self-service/category/destination-content/api-doc/points-of-interest/api-reference
+    String address = "/v1/reference-data/locations/pois/" + id;
+    wireMockServer.stubFor(get(urlEqualTo(address))
+        .willReturn(aResponse().withHeader("Content-Type", "application/json")
+        .withStatus(200)
+        .withBodyFile("poi_by_id_response.json")));
+
+    //When
+    PointOfInterest result = amadeus.referenceData.locations.pointOfInterest(id).get();
+
+    //Then
+    then(result).isNotNull();
+  }
+
+}
