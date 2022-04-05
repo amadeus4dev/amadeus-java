ee4d85005c0ea4784e3d0d1ac2a2ad57acd5c111
Juan Antonio Brena Moral <juanantonio.brenamoral@amadeus.com>
2022-04-05 13:10:33 +0200

    Adding IT test for pointsOfInterest

--- src/test/java/com/amadeus/shopping/activities/BySquareIT.java
+++ src/test/java/com/amadeus/shopping/activities/BySquareIT.java
@@ -1,4 +1,4 @@
-package com.amadeus.referenceData.locations.pointsOfInterest;
+package com.amadeus.shopping.activities;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
@@ -11,6 +11,7 @@ import com.amadeus.Amadeus;
 import com.amadeus.Params;
 import com.amadeus.exceptions.ClientException;
 import com.amadeus.exceptions.ResponseException;
+import com.amadeus.resources.Activity;
 import com.amadeus.resources.PointOfInterest;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.AfterEach;
@@ -54,17 +55,17 @@ public class BySquareIT {
   }
 
   @Test
-  public void given_client_when_call_points_of_interest_by_square_with_params_then_ok()
+  public void given_client_when_call_shopping_activities_by_square_with_params_then_ok()
       throws ResponseException {
 
     //Given
-    //https://developers.amadeus.com/self-service/category/destination-content/api-doc/points-of-interest/api-reference
-    String address = "/v1/reference-data/locations/pois/by-square"
+    //https://developers.amadeus.com/blog/introducing-tours-and-activities-api
+    String address = "/v1/shopping/activities/by-square"
         + "?east=2.177181&south=41.394582&north=41.397158&west=2.160873";
     wireMockServer.stubFor(get(urlEqualTo(address))
         .willReturn(aResponse().withHeader("Content-Type", "application/json")
         .withStatus(200)
-        .withBodyFile("pois_by_square_response_ok.json")));
+        .withBodyFile("activities_response_ok.json")));
 
     Params params = Params
         .with("north", "41.397158")
@@ -73,11 +74,10 @@ public class BySquareIT {
         .and("east", "2.177181");
 
     //When
-    PointOfInterest[] result = amadeus.referenceData.locations
-        .pointsOfInterest.bySquare.get(params);
+    Activity[] result = amadeus.shopping.activities.bySquare.get(params);
 
     //Then
-    then(result.length).isNotEqualTo(0);
+    then(result).isNotNull();
   }
 
   @Test
@@ -85,16 +85,16 @@ public class BySquareIT {
       throws ResponseException {
 
     //Given
-    String address = "/v1/reference-data/locations/pois/by-square";
+    String address = "/v1/shopping/activities/by-square";
     wireMockServer.stubFor(get(urlEqualTo(address))
-          .willReturn(aResponse().withHeader("Content-Type", "application/json")
-          .withStatus(400)
-          .withBody("")));
+        .willReturn(aResponse().withHeader("Content-Type", "application/json")
+        .withStatus(400)
+        .withBody("")));
 
     //When
     //Then
     assertThatThrownBy(() -> {
-      amadeus.referenceData.locations.pointsOfInterest.bySquare.get();
+      amadeus.shopping.activities.bySquare.get();
     }).isInstanceOf(ClientException.class);
   }
 
