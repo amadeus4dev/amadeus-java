0735fccd5e0b13e8378ba78f6e85f86fefb24887
Juan Antonio Brena Moral <juanantonio.brenamoral@amadeus.com>
2022-04-21 17:15:01 +0200

    Adding a Jbang script to verify versions

---
+++ .github/VersionLibraryVerifier.java
@@ -0,0 +1,68 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static java.util.function.Predicate.not;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.List;
+
+/**
+ * This Java class verify in the library that in the following files:
+ *
+ * - /gradle.properties
+ * - /src/main/java/com/amadeus/Amadeus.java
+ *
+ * The versions of the library continues with the same value.
+ */
+public class VersionLibraryVerifier {
+
+    public static void main(String[] args) throws IOException {
+
+        var userDirPath = new File(System.getProperty("user.dir"));
+        var specPath = userDirPath.getParent() + "/";
+
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(specPath + "/src/main/java/com/amadeus/Amadeus.java"));
+            String versionJava = allLines.stream()
+                .filter(str -> str.contains("VERSION"))
+                .limit(1)
+                .map(str -> {
+                    Integer position = str.indexOf("VERSION");
+                    String chuncked = str.substring(position + "VERSION".length()).trim();
+                    chuncked = chuncked.replace("=","").trim();
+                    chuncked = chuncked.replace(";","");
+                    chuncked = chuncked.replace("\"","");
+                    return chuncked;
+                })
+                .peek(System.out::println)
+                .findFirst()
+                .get();
+
+            Properties properties= new Properties();
+            properties.load(new FileInputStream(new File(specPath + "gradle.properties")));
+            String versionGradle = properties.get("VERSION_NAME").toString();
+            System.out.println(versionGradle);
+
+            //Assert
+            if(versionJava.equals(versionGradle)) {
+                System.out.println("Versions in the library are the same.");
+                System.exit(0);
+            } else {
+                System.out.println("Versions in the library are not the same.");
+                System.exit(-1);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
