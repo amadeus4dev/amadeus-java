8fa08e024a71de04785c457e4156f5c4a7b31a46
Juan Antonio Brena Moral <juanantonio.brenamoral@amadeus.com>
2022-06-01 12:46:58 +0200

    Refactoring the code

--- .github/VersionLibraryVerifier.java
+++ .github/VersionLibraryVerifier.java
@@ -1,17 +1,14 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
 import java.io.File;
-import java.io.IOException;
-import java.io.FileReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * This Java class verify in the library that in the following files:
@@ -26,45 +23,63 @@ import java.util.List;
  */
 public class VersionLibraryVerifier {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        String specDir = args[0];
-        File userDirPath = new File(System.getProperty("user.dir"));
-        String specPath = userDirPath.getParent() + "/" + specDir;
+        //Parameter
+        String path = args[0];
 
-        try {
-            List<String> allLines = Files.readAllLines(Paths.get(specPath + "/src/main/java/com/amadeus/Amadeus.java"));
-            String versionJava = allLines.stream()
-                .filter(str -> str.contains("VERSION"))
-                .limit(1)
-                .map(str -> {
-                    Integer position = str.indexOf("VERSION");
-                    String chuncked = str.substring(position + "VERSION".length()).trim();
-                    chuncked = chuncked.replace("=","").trim();
-                    chuncked = chuncked.replace(";","");
-                    chuncked = chuncked.replace("\"","");
-                    return chuncked;
-                })
-                .peek(System.out::println)
-                .findFirst()
-                .get();
+        //Parse line to extract the version value
+        Function<String, String> extractValue = str -> {
+          Integer position = str.indexOf("VERSION");
+          String chuncked = str.substring(position + "VERSION".length()).trim();
+          chuncked = chuncked.replace("=","").trim();
+          chuncked = chuncked.replace(";","");
+          chuncked = chuncked.replace("\"","");
+          return chuncked;
+        };
 
-            Properties properties= new Properties();
-            properties.load(new FileInputStream(new File(specPath + "gradle.properties")));
+        //Get library version from Java class (Amadeus.java)
+        Function<String, String> getJavaVersion = param -> {
+          try {
+            List<String> allLines = Files.readAllLines(Paths.get(param + "/src/main/java/com/amadeus/Amadeus.java"));
+            return allLines.stream()
+              .filter(str -> str.contains("VERSION"))
+              .limit(1)
+              .map(extractValue)
+              .peek(System.out::println)
+              .findFirst()
+              .orElseThrow(RuntimeException::new);
+          } catch (IOException e) {
+            return "999";
+          }
+        };
+
+        //Get library version from Gradle file (gradle.properties)
+        Function<String, String> getGradleVersion = param -> {
+          try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(new File(param + "gradle.properties")));
             String versionGradle = properties.get("VERSION_NAME").toString();
             System.out.println(versionGradle);
+            return versionGradle;
+          } catch (IOException e) {
+              return "998";
+          }
+        };
 
-            //Assert
-            if(versionJava.equals(versionGradle)) {
-                System.out.println("Versions in the library are the same.");
-                System.exit(0);
-            } else {
-                System.out.println("Versions in the library are not the same.");
-                System.exit(-1);
-            }
+        //Process
+        File userDirPath = new File(System.getProperty("user.dir"));
+        String directoryPath = userDirPath.getParent() + "/" + path;
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        String versionJava = getJavaVersion.apply(directoryPath);
+        String versionGradle = getGradleVersion.apply(directoryPath);
+
+        //Assert
+        if(versionJava.equals(versionGradle)) {
+            System.out.println("Versions in the library are the same.");
+            System.exit(0);
+        } else {
+            System.out.println("Versions in the library are not the same.");
             System.exit(-1);
         }
     }
