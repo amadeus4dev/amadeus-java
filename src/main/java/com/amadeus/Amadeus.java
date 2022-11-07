15ffcdd599867139d3e0b11041e547be9c1365c8
Anna Tsolakou <anna.tsolakou@amadeus.com>
2022-11-07 12:35:39 +0100

    release 6.4.0

--- src/main/java/com/amadeus/Amadeus.java
+++ src/main/java/com/amadeus/Amadeus.java
@@ -25,7 +25,7 @@ public class Amadeus extends HTTPClient {
   /**
    * The API version.
    */
-  public static final String VERSION = "6.3.0";
+  public static final String VERSION = "6.4.0";
 
   /**
    * <p>
