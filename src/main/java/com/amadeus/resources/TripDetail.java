300d4e06e640a05d6c094abec720221c916370c4
Anna Tsolakou <anna.tsolakou@amadeus.com>
2023-01-17 09:43:49 +0100

    update trip detail

--- src/main/java/com/amadeus/resources/TripDetail.java
+++ src/main/java/com/amadeus/resources/TripDetail.java
@@ -9,15 +9,12 @@ import lombok.ToString;
  */
 @ToString
 public class TripDetail extends Resource {
-  protected TripDetail() {}
-
   private @Getter Trip trip;
+
+    protected TripDetail() {}
 
   @ToString
   public class Trip {
-    protected Trip() {
-    }
-
     private @Getter String type;
     private @Getter String reference;
     private @Getter String creationDateTime;
@@ -32,32 +29,33 @@ public class TripDetail extends Resource {
     private @Getter Stakeholder[] stakeholders;
     private @Getter Price price;
     private @Getter Product[] products;
+
+    protected Trip() {}
+
   }
 
   @ToString
   public class Start {
-    protected Start() {}
-
     private @Getter String localDateTime;
     private @Getter String name;
     private @Getter String iataCode;
     private @Getter Address address;
+
+    protected Start() {}
   }
 
   @ToString
   public class End {
-    protected End() {}
-
     private @Getter String localDateTime;
     private @Getter String name;
     private @Getter String iataCode;
     private @Getter Address address;
+
+    protected End() {}
   }
 
   @ToString
   public class Address {
-    protected Address() {}
-
     private @Getter String category;
     private @Getter String[] lines;
     private @Getter String postalCode;
@@ -67,83 +65,83 @@ public class TripDetail extends Resource {
     private @Getter String postalBox;
     private @Getter String text;
     private @Getter String state;
+
+    protected Address() {}
   }
 
   @ToString
   public class TravelAgency {
-    protected TravelAgency() {}
-
     private @Getter String officeName;
     private @Getter Address address;
     private @Getter Phone phone;
     private @Getter Email email;
+
+    protected TravelAgency() {}
   }
 
   @ToString
   public class Phone {
-    protected Phone() {}
-
     private @Getter String category;
     private @Getter String countryCode;
     private @Getter String number;
+
+    protected Phone() {}
   }
 
   @ToString
   public class Email {
-    protected Email() {}
-
     private @Getter String category;
     private @Getter String address;
+
+    protected Email() {}
   }
 
   @ToString
   public class Stakeholder {
-    protected Stakeholder() {}
-
     private @Getter String id;
     private @Getter String nationality;
     private @Getter String passangerTypeCode;
     private @Getter String dateOfBirth;
     private @Getter Integer age;
     private @Getter Name name;
+
+    protected Stakeholder() {}
   }
 
   @ToString
   public class Name {
-    protected Name() {}
-
     private @Getter String firstName;
     private @Getter String lastName;
     private @Getter String title;
     private @Getter String middleName;
     private @Getter String prefix;
     private @Getter String suffix;
+
+    protected Name() {}
   }
 
   @ToString
   public class Price {
-    protected Price() {}
-
     private @Getter String currency;
     private @Getter String total;
     private @Getter String base;
     private @Getter String totalTaxes;
+
+    protected Price() {}
   }
 
   @ToString
   public class Product {
-    protected Product() {}
-
     private @Getter Air air;
     private @Getter Hotel hotel;
     private @Getter Car car;
     private @Getter Train train;
+
+    protected Product() {}
   }
 
   @ToString
   public class Air {
-    protected Air() {}
-
     private @Getter String confirmationNumber;
     private @Getter Baggage baggages;
     private @Getter Meal meal;
@@ -155,127 +153,127 @@ public class TripDetail extends Resource {
     private @Getter Operating operating;
     private @Getter Aircraft aircraft;
     private @Getter Seat[] seats;
+
+    protected Air() {}
   }
 
   @ToString
   public class Baggage {
-    protected Baggage() {}
-
     private @Getter String quantity;
     private @Getter Weight weight;
+
+    protected Baggage() {}
   }
 
   @ToString
   public class Weight {
-    protected Weight() {}
-
     private @Getter String amount;
+
+    protected Weight() {}
   }
 
   @ToString
   public class Meal {
-    protected Meal() {}
-
     private @Getter String code;
     private @Getter String description;
+
+    protected Meal() {}
   }
 
   @ToString
   public class DepartureAirportLocation {
-    protected DepartureAirportLocation() {}
-
     private @Getter String name;
     private @Getter Address address;
+
+    protected DepartureAirportLocation() {}
   }
 
   @ToString
   public class ArrivalAirportLocation {
-    protected ArrivalAirportLocation() {}
-
     private @Getter String name;
     private @Getter Address address;
+
+    protected ArrivalAirportLocation() {}
   }
 
   @ToString
   public class DepartureAir {
-    protected DepartureAir() {}
-
     private @Getter String iataCode;
     private @Getter String terminal;
     private @Getter String checkInEndTime;
     private @Getter String localDateTime;
+
+    protected DepartureAir() {}
   }
 
   @ToString
   public class ArrivalAir {
-    protected ArrivalAir() {}
-
     private @Getter String iataCode;
     private @Getter String terminal;
     private @Getter String localDateTime;
+
+    protected ArrivalAir() {}
   }
 
   @ToString
   public class Marketing {
-    protected Marketing() {}
-
     private @Getter Carrier carrier;
     private @Getter FlightDesignator flightDesignator;
+
+    protected Marketing() {}
   }
 
   @ToString
   public class Operating {
-    protected Operating() {}
-
     private @Getter Carrier carrier;
     private @Getter FlightDesignator flightDesignator;
+
+    protected Operating() {}
   }
 
   @ToString
   public class Carrier {
-    protected Carrier() {}
-
     private @Getter String name;
+
+    protected Carrier() {}
   }
 
   @ToString
   public class FlightDesignator {
-    protected FlightDesignator() {}
-
     private @Getter String carrierCode;
     private @Getter String flightNumber;
     private @Getter String operationalSuffix;
+
+    protected FlightDesignator() {}
   }
 
   @ToString
   public class Aircraft {
-    protected Aircraft() {}
-
     private @Getter String aircraftType;
     private @Getter String aircraftDescription;
+
+    protected Aircraft() {}
   }
 
   @ToString
   public class Seat {
-    protected Seat() {}
-
     private @Getter String number;
     private @Getter String cabin;
     private @Getter AssociationRef[] associationRefs;
+
+    protected Seat() {}
   }
 
   @ToString
   public class AssociationRef {
-    protected AssociationRef() {}
-
     private @Getter String id;
     private @Getter String type;
+
+    protected AssociationRef() {}
   }
 
   @ToString
   public class Hotel {
-    protected Hotel() {}
-
     private @Getter String confirmationNumber;
     private @Getter String checkInDate;
     private @Getter String checkOutDate;
@@ -287,65 +285,65 @@ public class TripDetail extends Resource {
     private @Getter Policies policies;
     private @Getter Guests guests;
     private @Getter Room room;
+
+    protected Hotel() {}
   }
 
   @ToString
   public class ContactHotel {
-    protected ContactHotel() {}
-
     private @Getter String phone;
+
+    protected ContactHotel() {}
   }
 
   @ToString
   public class Description {
-    protected Description() {}
-
     private @Getter String text;
+
+    protected Description() {}
   }
 
   @ToString
   public class Policies {
-    protected Policies() {}
-
     private @Getter Cancellation cancellation;
+
+    protected Policies() {}
   }
 
   @ToString
   public class Cancellation {
-    protected Cancellation() {}
-
     private @Getter Description description;
+
+    protected Cancellation() {}
   }
 
   @ToString
   public class Guests {
-    protected Guests() {}
-
     private @Getter Integer adults;
     private @Getter Integer[] childAge;
+
+    protected Guests() {}
   }
 
   @ToString
   public class Room {
-    protected Room() {}
-
     private @Getter String type;
     private @Getter TypeEstimated typeEstimated;
+
+    protected Room() {}
   }
 
   @ToString
   public class TypeEstimated {
-    protected TypeEstimated() {}
-
     private @Getter String category;
     private @Getter String beds;
     private @Getter String bedType;
+
+    protected TypeEstimated() {}
   }
 
   @ToString
   public class Car {
-    protected Car() {}
-
     private @Getter String confirmationNumber;
     private @Getter String serviceProviderName;
     private @Getter AssociatedEquipment[] associatedEquipments;
@@ -353,66 +351,66 @@ public class TripDetail extends Resource {
     private @Getter Dropoff dropoff;
     private @Getter Driver driver;
     private @Getter CarVehicle vehicle;
+
+    protected Car() {}
   }
 
   @ToString
   public class AssociatedEquipment {
-    protected AssociatedEquipment() {}
-
     private @Getter String name;
+
+    protected AssociatedEquipment() {}
   }
 
   @ToString
   public class Pickup {
-    protected Pickup() {}
-
     private @Getter String localDateTime;
     private @Getter Location location;
+
+    protected Pickup() {}
   }
 
   @ToString
   public class Dropoff {
-    protected Dropoff() {}
-
     private @Getter String localDateTime;
     private @Getter Location location;
+
+    protected Dropoff() {}
   }
 
   @ToString
   public class Location {
-    protected Location() {}
-
     private @Getter String iataCode;
     private @Getter Address address;
+
+    protected Location() {}
   }
 
   @ToString
   public class Driver {
-    protected Driver() {}
-
     private @Getter Contact[] contacts;
+
+    protected Driver() {}
   }
 
   @ToString
   public class Contact {
-    protected Contact() {}
-
     private @Getter Phone phone;
+
+    protected Contact() {}
   }
 
   @ToString
   public class CarVehicle {
-    protected CarVehicle() {}
-
     private @Getter String acrissCode;
     private @Getter String carModel;
     private @Getter Integer doors;
+
+    protected CarVehicle() {}
   }
 
   @ToString
   public class Train {
-    protected Train() {}
-
     private @Getter String confirmNbr;
     private @Getter String serviceProviderName;
     private @Getter String bookingClass;
@@ -424,33 +422,35 @@ public class TripDetail extends Resource {
     private @Getter String arrivalTrack;
     private @Getter Seat[] seats;
     private @Getter Vehicle vehicle;
+
+    protected Train() {}
   }
 
   @ToString
   public class Departure {
-    protected Departure() {}
-
     private @Getter String subType;
     private @Getter String name;
     private @Getter String iataCode;
+
+    protected Departure() {}
   }
 
   @ToString
   public class Arrival {
-    protected Arrival() {}
-
     private @Getter String subType;
     private @Getter String name;
     private @Getter String iataCode;
+
+    protected Arrival() {}
   }
 
   @ToString
   public class Vehicle {
-    protected Vehicle() {}
-
     private @Getter String vehicleType;
     private @Getter String code;
     private @Getter String number;
     private @Getter String displayName;
+
+    protected Vehicle() {}
   }
 }
